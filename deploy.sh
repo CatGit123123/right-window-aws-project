32ca381febe257ebd17b7855981e19784c838e5d
CatGit123123 <cthayer3@gmu.edu>
2025-11-24 04:45:18 +0000

    Update SOURCE_VERSION for deployment type
    helping a bro out

--- deploy.sh
+++ deploy.sh
@@ -420,13 +420,13 @@ EOF
         BUILD_IMAGE="aws/codebuild/amazonlinux-x86_64-standard:5.0"
         COMPUTE_TYPE="BUILD_GENERAL1_SMALL"
         PRIVILEGED_MODE="false"
-        SOURCE_VERSION="Leverg"  # Use pdf2html-subtree since buildspec only exists there
+        SOURCE_VERSION="Leverg-Security"  # Use pdf2html-subtree since buildspec only exists there
         BUILDSPEC_FILE="buildspec-unified.yml"
     else
         BUILD_IMAGE="aws/codebuild/amazonlinux2-x86_64-standard:5.0"
         COMPUTE_TYPE="BUILD_GENERAL1_LARGE"
         PRIVILEGED_MODE="true"
-        SOURCE_VERSION="Leverg"
+        SOURCE_VERSION="Leverg-Security"
         BUILDSPEC_FILE="buildspec-unified.yml"
     fi
 
